f9523d63476e27b84e409fd1424f092782477c7e
AnaghaCS <anaghasasidharan38@gmail.com>
2024-02-13 14:39:31 +0000

    templates setup

--- home/views.py
+++ home/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-      return render(request,'homes/Base.html')
+      return render(request,'commons/base.html')
